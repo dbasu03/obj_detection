3506b2ab04abe1acc92017763d67def310974968
dbasu03 <basudeepayan123@gmail.com>
2025-01-25 10:51:34 -0800

    changes

--- app.py
+++ app.py
@@ -8,29 +8,29 @@ from torchvision.models.detection import fasterrcnn_resnet50_fpn_v2, FasterRCNN_
 from torchvision.utils import draw_bounding_boxes
 
 weights = FasterRCNN_ResNet50_FPN_V2_Weights.DEFAULT
-categories = weights.meta["categories"] ## ['__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A', 'stopsign',]
-img_preprocess = weights.transforms() ## Scales values from 0-255 range to 0-1 range.
+categories = weights.meta["categories"] 
+img_preprocess = weights.transforms() 
 
 @st.cache_resource
 def load_model():
     model = fasterrcnn_resnet50_fpn_v2(weights=weights, box_score_thresh=0.5)
-    model.eval(); ## Setting Model for Evaluation/Prediction   
+    model.eval(); 
     return model
 
 model = load_model()
 
 def make_prediction(img): 
-    img_processed = img_preprocess(img) ## (3,500,500) 
-    prediction = model(img_processed.unsqueeze(0)) # (1,3,500,500)
-    prediction = prediction[0]                       ## Dictionary with keys "boxes", "labels", "scores".
+    img_processed = img_preprocess(img) 
+    prediction = model(img_processed.unsqueeze(0)) 
+    prediction = prediction[0] 
     prediction["labels"] = [categories[label] for label in prediction["labels"]]
     return prediction
 
-def create_image_with_bboxes(img, prediction): ## Adds Bounding Boxes around original Image.
-    img_tensor = torch.tensor(img) ## Transpose
+def create_image_with_bboxes(img, prediction):
+    img_tensor = torch.tensor(img) 
     img_with_bboxes = draw_bounding_boxes(img_tensor, boxes=prediction["boxes"], labels=prediction["labels"],
                                           colors=["red" if label=="person" else "green" for label in prediction["labels"]] , width=2)
-    img_with_bboxes_np = img_with_bboxes.detach().numpy().transpose(1,2,0) ### (3,W,H) -> (W,H,3), Channel first to channel last.
+    img_with_bboxes_np = img_with_bboxes.detach().numpy().transpose(1,2,0) 
     return img_with_bboxes_np
 
 ## Dashboard
@@ -40,8 +40,8 @@ upload = st.file_uploader(label="Upload Image Here:", type=["png", "jpg", "jpeg"
 if upload:
     img = Image.open(upload)
 
-    prediction = make_prediction(img) ## Dictionary
-    img_with_bbox = create_image_with_bboxes(np.array(img).transpose(2,0,1), prediction) ## (W,H,3) -> (3,W,H)
+    prediction = make_prediction(img) 
+    img_with_bbox = create_image_with_bboxes(np.array(img).transpose(2,0,1), prediction) 
 
     fig = plt.figure(figsize=(12,12))
     ax = fig.add_subplot(111)
